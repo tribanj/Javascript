dd59346a51d8a21337d3de960f8298c6a34ea8ec
Tribhuwan <tribhuwanja@gmail.com>
2023-09-24 14:32:12 +0530

    import export done

--- NodeJs/importExport/index.js
+++ NodeJs/importExport/index.js
@@ -1,2 +1,5 @@
-const add = require("./operator");
+const { add, sub, mul } = require("./operator");
+
 console.log(add(5, 5));
+console.log(sub(15, 5));
+console.log(mul(15, 5));
--- NodeJs/importExport/operator.js
+++ NodeJs/importExport/operator.js
@@ -2,4 +2,14 @@ const add = (a, b) => {
   return a + b;
 };
 
-module.exports = add;
+const sub = (a, b) => {
+  return a - b;
+};
+const mul = (a, b) => {
+  return a * b;
+};
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+
+module.exports = { add, sub, mul };
