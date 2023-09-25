b6a0904b2ea9d0d530137d8cbb394ba5f4183ac3
Tribhuwan <tribhuwanja@gmail.com>
2023-09-25 20:50:06 +0530

    nodmon done

--- NodeJs/npmMOd/index.js
+++ NodeJs/npmMOd/index.js
@@ -1,9 +1,8 @@
+// const chalk = require("chalk");
 
 // console.log(chalk.blue("Hello world!"));
 var validator = require("validator");
-var chalk = require("chalk");
 
 const res = validator.isEmail("tribhuwan@tribhuwan.com");
-console.log(
-  res ? chalk.green.inverse(res) : chalk.foregroundColorNames.inverse(res)
-);
+console.log(res);
+console.log("hello nodemon");
