818ff3f711f4ec2b117a5572bde5a3d4b59c056b
Tribhuwan <tribhuwanja@gmail.com>
2023-09-25 19:46:40 +0530

    roadmap of backend added

--- NodeJs/importExport/operator.js
+++ NodeJs/importExport/operator.js
@@ -13,3 +13,16 @@ const mul = (a, b) => {
 // module.exports.sub = sub;
 
 module.exports = { add, sub, mul };
+
+
+
+// tips for logic building
+// step 1 :-  dataflow (keep continue to dry run )
+//step 2:- 90-10 teaching || 
+
+//todos app (more complex what you want);
+//{
+// road map for backend
+//A programming language  and a database
+// 
+// }
