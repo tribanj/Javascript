d10cf38cdc9698d41da6bf5cdc2a809bf93c5226
Tribhuwan <tribhuwanja@gmail.com>
2023-09-26 23:12:14 +0530

    creating your own api services

--- NodeJs/httpServer/index.js
+++ NodeJs/httpServer/index.js
@@ -9,16 +9,24 @@
 // you should include an HTTP header with the correct content type
 
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
-  //   console.log(req.url);
+  fs.readFileSync(`${__dirname}/userApi/userapi.json`, "utf-8");
+  const objectData = JSON.parse(data);
+
+
   if (req.url == "/") {
     res.end("Hello from the home page");
   } else if (req.url == "/about") {
     res.end("Hello from the about page");
   } else if (req.url == "/contact") {
     res.end("Hello from the contact page");
+  } else if (req.url == "/userapi") {
+
+    res.writeHead(200, {"content-type" : "application/json"})
+    res.end(objectData[2].name);
   } else {
-    res.writeHead(404, {"content type": "text/html"});
+    res.writeHead(404, { "content type": "text/html" });
     res.end(" <h1>Error! page not Found</h1>");
   }
 });
