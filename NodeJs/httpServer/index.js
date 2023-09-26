b6ff0295ccfa16e43b5ed61759e99f3c898ed7e0
Tribhuwan <tribhuwanja@gmail.com>
2023-09-26 22:20:25 +0530

    Routing in nodejs complete

--- NodeJs/httpServer/index.js
+++ NodeJs/httpServer/index.js
@@ -9,10 +9,19 @@
 // you should include an HTTP header with the correct content type
 
 const http = require("http");
-
 const server = http.createServer((req, res) => {
-  res.end("Hello from the ather sides TB bhai ");
+  //   console.log(req.url);
+  if (req.url == "/") {
+    res.end("Hello from the home page");
+  } else if (req.url == "/about") {
+    res.end("Hello from the about page");
+  } else if (req.url == "/contact") {
+    res.end("Hello from the contact page");
+  } else {
+    res.writeHead(404, {"content type": "text/html"});
+    res.end(" <h1>Error! page not Found</h1>");
+  }
 });
-server.listen(8000, "127.0.0.1", ()=>{
-    console.log("listning on port 8000")
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listning on port 8000");
 });
