370c4fd831f5a03c5df522f57ee4b46b2d090b16
Tribhuwan <tribhuwanja@gmail.com>
2023-09-28 01:03:06 +0530

    dynamic quotes page is ready

--- NodeJs/nodeMailer/app.js
+++ NodeJs/nodeMailer/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+
 let PORT = 5000;
 app.get("/", (req, res) => {
   res.send("I am server");
@@ -8,7 +9,7 @@ app.get("/", (req, res) => {
 
 const start = async () => {
   try {
-    app.listen(PORt, () => {
+    app.listen(PORT, () => {
       console.log(`I am live on port no. ${PORT}`);
     });
   } catch (error) {}
